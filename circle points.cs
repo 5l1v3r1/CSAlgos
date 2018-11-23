d669932bb367f6b7ff604010c298b1dbf47ed18b
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2018-11-23 15:23:44 -0500

    Update circle points.cs

--- circle points.cs
+++ circle points.cs
@@ -1,8 +1,15 @@
-public static Point[] CirclePoints(int radius, int centerx, inter centery)
+public static Point[] CirclePoints(double radius, double centerx, double centery)
 {
-	Point[] output = new Point[360];
-	for ()
+	radius = radius <= 0 ? 1 : radius;
+	Point[] output = new Point[359];
+	for (int i = 0; i < 359; i++)
 	{
+		output[i] = new Point(Convert.ToInt32((Math.Cos(DegToRad(i))*r) + centerx), Convert.ToInt32((Math.Sin(DegToRad(i)) * r) + centery));
 	}
 	return output;
+}
+public static double DegToRad (double degrees)
+{
+    double radians = (Math.PI / 180) * degrees;
+    return (radians);
 }
