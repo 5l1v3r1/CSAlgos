c075dfe59aa37b47edcf8497bf60a86154c815f0
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2018-11-28 19:51:16 -0500

    Update passwordgen.cs

--- passwordgen.cs
+++ passwordgen.cs
@@ -1,36 +1,47 @@
 public static string Password(string input)
 {
-	//for later stuff
 	input = input.ToLower();
 	//leet
-	input.Replace("a" "4");
-	input.Replace("i" "1");
-	input.Replace("e" "3");
-	input.Replace("l" "7");
-	input.Replace("g" "6");
-	input.Replace("s" "5");
-	input.Replace("z" "2");
+	input.Replace('a', '4');
+	input.Replace('i', '1');
+	input.Replace('e', '3');
+	input.Replace('l', '7');
+	input.Replace('g', '6');
+	input.Replace('s', '5');
+	input.Replace('z', '2');
+	Console.WriteLine(input);
 	//random symbols
-	string sym = "~!@#$%^&*()_+{}|:\"<>?`-=[]\;\',./\\";
+	char[] sym = "~!@#$%^&*()_+{}|:\"<>?`-=[]\\;\',./".ToCharArray();
+	Random rng = new Random();
 	for (int i = 0; i != 10; i++)
 	{
-		input[new Random().Next(0, input.Length)] = sym[new Random().Next(0, sym.Length)];
+		int r1 = rng.Next(0, input.Length);
+		int r2 = rng.Next(0, input.Length);
+		char[] zz = input.ToCharArray();		
+		zz[r1] = sym[r2];
+		input = new string(zz);
 	}
+	Console.WriteLine(input);
 	//random caps
-	for (int i = 0; i != input.Length/2; i++)
+	for (int i = 0; i != input.Length/4; i++)
 	{
-		input[i] = input[i].ToUpper();
+		char z = char.ToUpper(input[i]);
+		char[] zz = input.ToCharArray();	
+		zz[i] = z;
+		input = new string(zz);
 	}
+	Console.WriteLine(input);
 	//add more symbols
-	input = string.Format("{1}{1}{0}{1}{1}", input, sym[new Random().Next(0, sym.Length)]);
+	input = string.Format("{2}{1}{0}{3}{4}", input, sym[rng.Next(0, sym.Length)], sym[rng.Next(0, sym.Length)], sym[rng.Next(0, sym.Length)], sym[rng.Next(0, sym.Length)]);
+	Console.WriteLine(input);
 	//salt
-	string salt = RandStr(new Random().Next(5, 11));
+	string salt = RandStr(rng.Next(5, 11));
 	List<string> x = new List<string>();
 	foreach(char i in input)
 	{
 		x.Add(i.ToString());
 	}
-	int[] index = {new Random().Next(0, input.Length), new Random().Next(0, input.Length), new Random().Next(0, input.Length), new Random().Next(0, input.Length)};
+	int[] index = {rng.Next(0, input.Length), new Random().Next(0, input.Length), new Random().Next(0, input.Length), new Random().Next(0, input.Length)};
 	input = "";
 	foreach (int i in index)
 	{
@@ -46,7 +57,7 @@ public static string RandStr(int length)
 {
     string result = string.Empty;
     Random random = new Random((int)DateTime.Now.Ticks);
-    List<string> characters = new List<string>() { };
+    List<string> characters = new List<string>();
     for (int i = 48; i < 58; i++)
     {
         characters.Add(((char)i).ToString());
@@ -62,7 +73,7 @@ public static string RandStr(int length)
     for (int i = 0; i < length; i++)
     {
         result += characters[random.Next(0, characters.Count)];
-        Thread.Sleep(1);
+        System.Threading.Thread.Sleep(1);
     }
     return result;
 }
